e8a064b385d838b05a90c9eaf8c9e58e517ca15d
Akashgupta <skygupta2504@gmail.com>
2024-09-10 16:29:39 +0530

    env variable added

--- server/index.js
+++ server/index.js
@@ -4,19 +4,22 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+import dotenv from 'dotenv';
+dotenv.config();
 const app=express();
+const CONNECTION_URL=process.env.REAL_CONNECTION;
+// console.log(CONNECTION_URL);
 app.use(bodyParser.json({limit : "30mb", extended : true}))
 app.use(bodyParser.urlencoded({limit : "30mb", extended : true}))
 app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-const CONNECTION_URL='mongodb+srv://akash_gupta:Allsmall10@mecluster.ew66d2j.mongodb.net/?retryWrites=true&w=majority&appName=Mecluster'
 const PORT= process.env.PORT || 3000;
 mongoose.connect(CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Sever on : ${PORT}`)))
 .catch((error) => console.log(error.message));
 
-
+ 
 
 
 
